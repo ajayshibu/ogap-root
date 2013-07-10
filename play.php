c5399eddb9935beb9484f67ddd9a5359b50dfd12
ajayshibu <ajay.shibu.ben@gmail.com>
2013-07-10 12:56:21 +0530

    change in index

--- play.php
+++ play.php
@@ -1,1 +1,9 @@
-<?php echo('<h1>This is the landing Page of O-Gap</h1>'); ?>
+<?php echo('<h1>This is the landing Page of O-Gap</h1>');
+
+define('__ROOT__',dirname(__FILE__));
+
+$index = file_get_contents(__ROOT__.'/projects/ogap-template/index.html');
+
+echo($index);
+ ?>
+
